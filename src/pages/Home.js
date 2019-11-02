eba03372ffc059d9d35693d8c0713a082fa0def8
Nkupp <navneelkuppili@Navneels-MacBook-Pro.local>
2019-11-01 22:09:51 -0400

    basic project setup forked from react scaffolding

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Hello from HOME! THIS is the home where you belong!
+        </div>
+    )
+}
+
+export default Home;
